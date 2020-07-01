4a1f41a4f6ec661c817ba033ee6936c11d6375c6
Mohamed Beshri Amer <mohamed.perry351@gmail.com>
2020-06-30 18:24:04 -0700

    EnvoirmentVariables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0J42vdWU3OWwjsBaxi668cOZsmn1mAPY",
+    authDomain: "atomic-vault-280814.firebaseapp.com",
+    databaseURL: "https://atomic-vault-280814.firebaseio.com",
+    projectId: "atomic-vault-280814",
+    storageBucket: "atomic-vault-280814.appspot.com",
+    messagingSenderId: "713454138827",
+    appId: "1:713454138827:web:e6c69239f43f48de8e724e",
+    measurementId: "G-28CTSZL7HF"
+  }
 };
